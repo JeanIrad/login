72a7993d61a33c1e0cc0dbb5af6eb6b80a1520b4
Jean de Dieu <jado.milton@gmail.com>
2023-12-22 00:51:59 +0200

    changed js file

--- js/main.js
+++ js/main.js
@@ -7,12 +7,8 @@ const docTitle = document.querySelector("title");
 
 inputs.forEach((input) =>
   input.addEventListener("keyup", (e) => {
-    let checkName = 0,
-      checkPass = 0;
-    if (input.name === "username") checkName = e.target.value.trim().length;
-
-    if (input.name === "password") checkPass = e.target.value.trim().length;
-    if (checkName < 4 || checkPass < 8) {
+    let check = e.target.value.trim().length;
+    if (check < 8) {
       input.classList.add("invalid");
     } else {
       input.classList.remove("invalid");
